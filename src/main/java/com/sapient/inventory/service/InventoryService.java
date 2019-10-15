765cc7423e9778e0ed107203099bed36ba6e5c0c
Balaram Samal <balsamal@publicisgroupe.net>
2019-10-15 16:45:50 +0530

    modified repo2

--- src/main/java/com/sapient/inventory/service/InventoryService.java
+++ src/main/java/com/sapient/inventory/service/InventoryService.java
@@ -2,10 +2,8 @@ package com.sapient.inventory.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
--- src/main/java/com/sapient/inventory/web/rest/InventoryController.java
+++ src/main/java/com/sapient/inventory/web/rest/InventoryController.java
@@ -1,21 +1,18 @@
 package com.sapient.inventory.web.rest;
 
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.sapient.inventory.InventoryResponse;
 import com.sapient.inventory.domain.Inventory;
 import com.sapient.inventory.domain.Product;
@@ -41,14 +38,14 @@ public class InventoryController {
 	}
 
 	@PutMapping("/items/{id}")
-	public ResponseEntity<InventoryResponse> updateItemList(@RequestParam String id,
+	public ResponseEntity<InventoryResponse> updateItemList(@PathVariable String id,
 														 @RequestBody Inventory inventoryPojo){
 		InventoryResponse ir = inventoryService.updateInventoryItem(id, inventoryPojo);
 		return new ResponseEntity<InventoryResponse>(ir, HttpStatus.ACCEPTED);
 	}
 
 	@DeleteMapping("/items/{id}")
-	public ResponseEntity<InventoryResponse> deleteItemList(@RequestParam String id) {
+	public ResponseEntity<InventoryResponse> deleteItemList(@PathVariable String id) {
 		InventoryResponse ir = inventoryService.deleteInventoryItem(id);
 		return new ResponseEntity<InventoryResponse>(ir, HttpStatus.OK);
 	}
@@ -60,9 +57,9 @@ public class InventoryController {
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/products/ids/{ids}/task/{task}")
-	public ResponseEntity<String> updateInventoryProducts(@RequestParam String products,
-														  @RequestParam String task) {
-		InventoryResponse ir  = inventoryService.updateInventoryProducts(products, task);
+	public ResponseEntity<String> updateInventoryProducts(@PathVariable String ids,
+			@PathVariable String task) {
+		InventoryResponse ir  = inventoryService.updateInventoryProducts(ids, task);
 		return ResponseEntity.ok().body("Done");
 		
 	}
